e87bf38c422f1c3f451e2f15fa90c5a710aafbf6
Yashna Chugh <yashnachugh2004@gmail.com>
2025-10-01 23:55:02 +0530

    Update smtp_test.py

--- smtp_test.py
+++ smtp_test.py
@@ -5,14 +5,14 @@ from email.mime.multipart import MIMEMultipart
 # SMTP server configuration (Gmail example)
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
-sender_email = "yashnachugh2004@gmail.com"
-sender_password = "uojw mkbi gssd zksd"  # Use App Password if 2FA is enabled
+sender_email = "sender_email@example.com"
+sender_password = "app_password"  # Use App Password if 2FA is enabled
 receiver_email = "receiver_email@example.com"
 
 # Create email message
 message = MIMEMultipart()
-message["From"] = "yashnachugh2004@gmail.com"
-message["To"] = "bajajaryan20@gmail.com"
+message["From"] = "sender_email@example.com"
+message["To"] = "reciever_email@example.com"
 message["Subject"] = "Test Email from Python SMTP"
 
 body = "Hello! This is a test email sent using Python and SMTP."
@@ -23,9 +23,10 @@ try:
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.starttls()  # Secure connection
     server.login(sender_email, sender_password)
-    server.sendmail("yashnachugh2004@gmail.com", "bajajaryan20@gmail.com", message.as_string())
+    server.sendmail(sender_mail, reciever_mail, message.as_string())
     print("Email sent successfully!")
 except Exception as e:
     print(f"Error: {e}")
 finally:
     server.quit()
+
